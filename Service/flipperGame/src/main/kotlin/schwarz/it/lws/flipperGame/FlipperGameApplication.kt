9560cb0a8deb77b0183cc3ff8c8a6e579181848d
Riza Shabanaj <riza.shabanaj@mmmake.com>
2025-03-31 09:52:00 +0200

    added project with springboot kotlin

---
+++ Service/flipperGame/src/main/kotlin/schwarz/it/lws/flipperGame/FlipperGameApplication.kt
@@ -0,0 +1,11 @@
+package schwarz.it.lws.flipperGame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlipperGameApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlipperGameApplication>(*args)
+}
---
+++ Service/flipperGame/src/test/kotlin/schwarz/it/lws/flipperGame/FlipperGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package schwarz.it.lws.flipperGame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlipperGameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
